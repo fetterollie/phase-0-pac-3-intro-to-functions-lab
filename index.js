b4f1121c9fcae4c2c4d3834f12a9411c747d6a0d
Jonathan <jonathan.fetterolf@gmail.com>
2023-05-02 13:49:17 -0400

    complete

--- index.js
+++ index.js
@@ -16,6 +16,11 @@ function logShout(string) {
     return console.log(string.toLowerCase());
  }
 function sayHiToHeadphonedRoommate(string) {
+    /*
+    If the string that is passed into the function is all lowercase, the function should return "I can't hear you!"
+    If the string that is passed into the function is all uppercase, the function should return "YES INDEED!"
+    If the string that is passed into the function is equal to "Let's have dinner together!", the function should return "I would love to!"
+    */
     if (string === string.toLowerCase())
         return "I can't hear you!"
     else if (string === string.toUpperCase())
@@ -23,8 +28,3 @@ function sayHiToHeadphonedRoommate(string) {
     else if (string === "Let's have dinner together!")
         return "I would love to!";
  }
-/*
-If the string that is passed into the function is all lowercase, the function should return "I can't hear you!"
-If the string that is passed into the function is all uppercase, the function should return "YES INDEED!"
-If the string that is passed into the function is equal to "Let's have dinner together!", the function should return "I would love to!"
-*/
